ea21e000d81eed1bdaf5f0cbabf7d6de8f13b810
krzysztof.matyjaszczyk <krzysztof.matyjaszczyk@gmail.com>
2024-05-25 10:05:44 +0200

    Initial Commit

---
+++ km-lab3-zad2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  km-lab3-zad2
+//
+//  Created by student on 25/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ km-lab3-zad2/km_lab3_zad2App.swift
@@ -0,0 +1,17 @@
+//
+//  km_lab3_zad2App.swift
+//  km-lab3-zad2
+//
+//  Created by student on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct km_lab3_zad2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
